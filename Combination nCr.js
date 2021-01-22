add2d508688dc574692883d1cb4344fadd8743c4
Mohamed Ahmed <mohamedahmed315@hotmail.com>
2021-01-22 18:52:00 +0200

    Add files via upload

---
+++ Combination nCr.js
@@ -0,0 +1,21 @@
+
+let factorial = function(number){
+
+    let fact = 1;
+    for (let i = 1; i <= number; i++){
+        fact = fact * i;
+    }
+    return fact
+}
+
+let combination = function(n, r){
+    let factN = factorial(n);
+    let factR = factorial(r);
+
+    let nCr = factN / (factorial(n-r) * factR);
+
+    return nCr;
+}
+
+
+console.log(combination(5, 3));
